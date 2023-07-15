6c589df515f16faed0a4d65e0ae9c27c352b3df0
caymankun <111965236+caymankun@users.noreply.github.com>
2023-07-15 18:19:57 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-const logolink = document.getElementById('c-logo');
-function logo(){
-    
-}
+$(function(){
+    $('a[href^="#"]').click(function(){
+      var adjust = 0;
+      var speed = 400;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top + adjust;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+    });
+});
